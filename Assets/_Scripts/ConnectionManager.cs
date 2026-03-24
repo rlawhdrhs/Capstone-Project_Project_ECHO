7106570e48af518f9a3ffe108d780b02478dbe80
rlawhdrhs <56989625+rlawhdrhs@users.noreply.github.com>
2026-03-24 17:13:18 +0900

    잠입자 및 추격자 스폰

--- Assets/_Scripts/ConnectionManager.cs
+++ Assets/_Scripts/ConnectionManager.cs
@@ -6,6 +6,10 @@ public class ConnectionManager : MonoBehaviour
 {
     private NetworkRunner _runner;
 
+    [Header("플레이어 프리팹 할당")]
+    public NetworkPrefabRef chaserPrefab;      // 추격자 프리팹
+    public NetworkPrefabRef infiltratorPrefab; // 잠입자 프리팹
+
     // 서버 접속 시도
     async void Start()
     {
@@ -14,33 +18,49 @@ public class ConnectionManager : MonoBehaviour
 
     private async Task StartConnection()
     {
-        // 1. NetworkRunner 컴포넌트 가져오기
         _runner = gameObject.GetComponent<NetworkRunner>();
         if (_runner == null)
         {
             _runner = gameObject.AddComponent<NetworkRunner>();
         }
 
-        // 2. 입력 처리 활성화
         _runner.ProvideInput = true;
 
-        // 3. 공유 모드 접속 세팅
         var startGameArgs = new StartGameArgs()
         {
             GameMode = GameMode.Shared,
             SessionName = "VR_Stealth_Room",
             SceneManager = gameObject.GetComponent<NetworkSceneManagerDefault>() ?? gameObject.AddComponent<NetworkSceneManagerDefault>()
         };
+
         Debug.Log("서버 접속 시도 중...");
         var result = await _runner.StartGame(startGameArgs);
 
         if (result.Ok)
         {
             Debug.Log("공유 모드 방 접속 성공! 현재 세션: " + _runner.SessionInfo.Name);
+            SpawnPlayer(); // 접속 성공 시 스폰 함수 실행
         }
         else
         {
             Debug.LogError("접속 실패: " + result.ShutdownReason);
+        }
+    }
+
+    // 접속 순서에 따라 비대칭 스폰을 진행하는 함수
+    private void SpawnPlayer()
+    {
+        if (_runner.IsSharedModeMasterClient)
+        {
+            Debug.Log("추격자(AI) 스폰");
+            // 추격자 소환 (위치: 0,0,0)
+            _runner.Spawn(chaserPrefab, Vector3.zero, Quaternion.identity, _runner.LocalPlayer);
+        }
+        else
+        {
+            Debug.Log("잠입자(VR) 스폰");
+            // 잠입자 소환 (위치: 약간 옆인 2,0,0에 소환해서 겹치지 않게 함)
+            _runner.Spawn(infiltratorPrefab, new Vector3(2, 0, 0), Quaternion.identity, _runner.LocalPlayer);
         }
     }
 }
